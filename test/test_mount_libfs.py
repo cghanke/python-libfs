1127252f62654cdbedefa924177e818b03c5ac8d
Christof Hanke <github@induhviduals.de>
2017-04-20 20:02:37 +0200

    test_mount_libfs.py:  beautify it and make pylint a bit more happy
    all warnings cannot be silenced without obsolete code. So keep them.

--- test/test_mount_libfs.py
+++ test/test_mount_libfs.py
@@ -15,8 +15,11 @@ import unittest
 LIBFS_BIN = "scripts/libfs.py"
 LIBFS_MNT = "./test/mnt"
 
-class TestBase(unittest.TestCase) :
-    
+class TestBase(unittest.TestCase):
+    """
+    Base TestCase to be used by the plugin-TestCases
+    """
+
     EXISTING_FILE = "%s/NADA" % LIBFS_MNT
     EXISTING_DIR = "%s/NADA" % LIBFS_MNT
     LIBFS_SRC_DIR = "./test/data/src"
@@ -25,23 +28,26 @@ class TestBase(unittest.TestCase) :
     LIBFS_LOG_CFG = "./test/logging.cfg"
 
     @classmethod
-    def setUpClass(self):
+    def setUpClass(cls):
         """
         create a copy of the db and mount libfs in the background
         """
         # create copy of db
-        shutil.copyfile(self.LIBFS_DB_ORIG,  self.LIBFS_DB)
+        shutil.copyfile(cls.LIBFS_DB_ORIG, cls.LIBFS_DB)
         # mount libfs
-        self.mount_proc = subprocess.Popen([LIBFS_BIN, "--logconf", self.LIBFS_LOG_CFG, "mount", self.LIBFS_DB,  LIBFS_MNT], stderr=subprocess.PIPE,  stdout=subprocess.PIPE)
+        cls.mount_proc = subprocess.Popen([LIBFS_BIN, "--logconf", cls.LIBFS_LOG_CFG, "mount",
+                                           cls.LIBFS_DB, LIBFS_MNT], stderr=subprocess.PIPE,
+                                          stdout=subprocess.PIPE)
         # check if mount worked
         time.sleep(0.5)
-        
-        if self.mount_proc.poll() != None:
-            output,  outerr = self.mount_proc.communicate()
-            raise RuntimeError("Mount failed with rc=%s. output=%s, outerr=%s" % (self.mount_proc.poll(),  output,  outerr))
-    
+
+        if cls.mount_proc.poll() != None:
+            output, outerr = cls.mount_proc.communicate()
+            raise RuntimeError("Mount failed with rc=%s. output=%s, outerr=%s" %\
+                               (cls.mount_proc.poll(), output, outerr))
+
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
         """
         unmount libfs
         """
@@ -51,41 +57,45 @@ class TestBase(unittest.TestCase) :
             subprocess.check_call(['fusermount', '-z', '-u', LIBFS_MNT])
         assert not os.path.ismount(LIBFS_MNT)
         # close the internal fileobjects
-        self.mount_proc.communicate()
-        
+        cls.mount_proc.communicate()
+
     def test_walk(self):
         """
         just walk through the whole tree
         """
-        for root,  dirs,  files in os.walk(LIBFS_MNT):
+        for root, dirs, files in os.walk(LIBFS_MNT):
             pass
-    
+
     def test_file_mv(self):
         """
         rename a file
         """
-        shutil.move(self.EXISTING_FILE,  self.NON_EXISTING_FILE)
+        shutil.move(self.EXISTING_FILE, self.NON_EXISTING_FILE)
         shutil.move(self.NON_EXISTING_FILE, self.EXISTING_FILE)
 
-    @unittest.expectedFailure
     def test_file_cp(self):
         """
-        copy a file, expected to fail
+        copy a file, expected to fail.
+        Copying a file into libfs is not supported
+        Likewise create.
         """
-        with self.assertRaises(FileNotFoundError) as cm:
-            shutil.copyfile(self.EXISTING_FILE,  self.NON_EXISTING_FILE)
-        
-        the_exception = cm.exception
-        self.assertEqual(the_exception.errno,  2)
-        
+        with self.assertRaises(Exception) as excep:
+            shutil.copyfile(self.EXISTING_FILE, self.NON_EXISTING_FILE)
+
+        the_exception = excep.exception
+        self.assertEqual(the_exception.errno, 38)
+
     def test_dir_mv(self):
         """
         Move a dir from, thus changing the metadata in the db.
         """
-        shutil.move(self.EXISTING_DIR,  self.NON_EXISTING_DIR)
-        shutil.move( self.NON_EXISTING_DIR,  self.EXISTING_DIR)
-    
+        shutil.move(self.EXISTING_DIR, self.NON_EXISTING_DIR)
+        shutil.move(self.NON_EXISTING_DIR, self.EXISTING_DIR)
+
 class ID3Test(TestBase):
+    """
+    Actual test using the id3-plugin
+    """
     LIBFS_SRC_DIR = "./test/data/src"
     LIBFS_DB_ORIG = "./test/data/testdb.orig"
     LIBFS_DB = "./test/data/testdb"
@@ -94,5 +104,5 @@ class ID3Test(TestBase):
     NON_EXISTING_FILE = "%s/A Cappella/Artist A/2000/Album A/1 -- Track NON.mp3" % LIBFS_MNT
     NON_EXISTING_DIR = "%s/A Cappella/Artist A/2000/Album NON" % LIBFS_MNT
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     unittest.main()
