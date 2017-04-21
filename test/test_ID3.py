5e44b5ab103f8704a556e4bedcf6813ce6fbb790
Christof Hanke <github@induhviduals.de>
2017-04-21 11:05:43 +0200

    test: rename src dir of ID3
    Now also other plugins may be incoporated

--- test/test_ID3.py
+++ test/test_ID3.py
@@ -9,7 +9,7 @@ class ID3Test(TestBase):
     Actual test using the id3-plugin
     """
     TYPE = "id3"
-    LIBFS_SRC_DIR = "./test/data/src"
+    LIBFS_SRC_DIR = "./test/data/id3"
     LIBFS_DB = "./test/data/testdb"
     EXISTING_FILE = "%s/A Cappella/Artist A/2000/Album A/1 -- Track A.mp3" % TestBase.LIBFS_MNT
     EXISTING_DIR = "%s/A Cappella/Artist A/2000/Album A" % TestBase.LIBFS_MNT
